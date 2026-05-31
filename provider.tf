bf80332afe55600be62e4643cdd5165e61ba6e4b
Uddhav18 <154421497+Uddhav18@users.noreply.github.com>
2026-05-31 13:41:34 +0530

    Rename account to account_name in provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "snowflake" {
-  account   = var.snowflake_account
+  account_name   = var.snowflake_account
   user      = var.snowflake_user
   password  = var.snowflake_password
   role      = var.snowflake_role
